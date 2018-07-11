2f42abca0953f74f2ba1e10a0c4e395e9df7ef8f
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:40:45 -0400

    Add files via upload

---
+++ ForEachLoop-example.ps1
@@ -0,0 +1,23 @@
+﻿$servernames = import-csv -path e:\test.csv
+foreach ($servername in $servernames)
+{
+"This is "+ $servername.hostname
+"this is "+ $servername.ip
+}
+
+
+#
+    for ($x=0; $x -lt $DestinationServer.Length; $x++)
+
+        { 
+         $DBLocation = ("\\"+ $DestinationServer[$x]+"\D$\Test\IBMS\SIT6CL\*")
+         $DBLocation
+
+         $DServer = $DestinationServer[$x]
+         Write-Host "Destination Server is: " $DServer
+         Write-Verbose -Verbose "Copying SIT6CL from $SourceLocation TO $DServer server"
+         #Remove-Item -Path $DBLocation -Force -Recurse -Verbose
+         #Robocopy.exe $SourceLocation $CTXFolder"\SIT6CL" /R:0 /W:0 /MT:32 /DCOPY:T /COPY:DAT /MIR /LOG+:D:\test\IBMS\Log\SIT6CL.txt
+}
+
+Get-Date
---
+++ foreach_for_loop script.ps1
@@ -0,0 +1,27 @@
+﻿$MagicTaskServers = "AGPJMT504", "AGPJMT500"
+$DBNames = "SIT6CL", "SIT6", "SIT6C"
+$count = ($DBNames.Count) -1
+$count
+
+
+
+for ($i=0; $i -lt $count; $i++) 
+{
+    write-host -foregroundcolor yellow $i"." $DBNames[$i] 
+}
+      
+ foreach ($MagicTaskServer in $MagicTaskServers)
+        {
+          Write-Host "`n$MagicTaskServer" -ForegroundColor Cyan `n
+          #"This is in foreach loop: $DBNames[$i]   "
+          
+            
+            foreach ($DBName in $DBNames)
+                {
+                    #"In 2nd foreach loop  $DBName"
+                   "Start the services on $DBName"
+                }
+        
+        
+        }
+
