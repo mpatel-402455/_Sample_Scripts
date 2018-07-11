4bab21065738ad4cf96a1976e4d1cf714c70716e
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:41:25 -0400

    Add files via upload

---
+++ Get-characters-sympbols.ps1
@@ -0,0 +1,17 @@
+﻿$items = 356..5000
+
+foreach ($item in $items)
+    {
+        $item
+        $greenCheck = @{
+        Object = [Char] $item           #AUM: 2768# 0x0AD0
+        ForegroundColor = 'Green'
+        NoNewLine = $true
+            }
+
+Write-Host "Status check... " -NoNewline
+#Start-Sleep -Seconds 1
+Write-Host @greenCheck
+Write-Host " (Done)"
+    }
+
