14d3d7cda7fef665b3382ab9f0395759c34da416
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 19:35:40 -0400

    Add files via upload

---
+++ Change-DNSSuffix.ps1
@@ -0,0 +1,23 @@
+﻿
+
+## FOLLOWING works locally 
+# 
+([WMIClass]"Win32_NetworkAdapterConfiguration").SetDNSSuffixSearchOrder(("prod.lab.ca", "lab.root", "lab.ca", "int.lab.ca", "dev.lab.ca", "production.lab.ca", "qalab.ca"))
+$a = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled = True"
+$a.DNSDomainSuffixSearchOrder
+
+
+
+#2 
+([WMIClass]"Win32_NetworkAdapterConfiguration").SetDNSSuffixSearchOrder(("prod.lab.ca", "domain.root", "lab.ca", "int.lab.ca", "dev.lab.ca", "production.lab.ca", "qalab.ca"))
+$a = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled = True"
+$a.DNSDomainSuffixSearchOrder
+
+
+
+#following are DOS command to change dns suffix on Windows 2003
+
+reg query HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v "SearchList"
+
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v "SearchList" /d "prod.lab.ca,lab.root,lab.ca,int.lab.ca,dev.lab.ca" /f
+
